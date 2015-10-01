32d3091f86fd2329c39733f616a1550dac48fdf3
Jeremy Parker <jpp39@cam.ac.uk>
2015-10-01 16:39:52 +0100

    small fixes

---
+++ Assets/shopcloser.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class shopcloser : MonoBehaviour {
+
+    public void Close()
+    {
+        if (SaucerPlayer.instance.tractor != upgradelevel.none && SaucerPlayer.instance.cargo != upgradelevel.none)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
